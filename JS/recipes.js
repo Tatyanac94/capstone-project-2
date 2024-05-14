42e127bb4ae32c337c6f5c9facb011a10c514686
Tatyana Cuttino <ms.cuttino94.com>
2024-05-14 01:42:23 -0400

    removed fetch request for ingredients

--- JS/recipes.js
+++ JS/recipes.js
@@ -2,7 +2,7 @@ document.getElementById('searchButton').addEventListener('click', search);
               
               function search() {
                   const searchTerm = encodeURIComponent(document.getElementById('searchInput').value);
-                  const ingredients = encodeURIComponent(document.getElementById('ingredientsInput').value);
+                //   const ingredients = encodeURIComponent(document.getElementById('ingredientsInput').value);
                   let baseURL = "https://api.api-ninjas.com/v1/cocktail";
                   let myKey = "Fvgwx0GoSoc8O88Lryedl5EJyfpvRtdh9Zlbjr1k";
                   let url = `${baseURL}?name=${searchTerm}&X-Api-Key=${myKey}`;
@@ -32,42 +32,3 @@ document.getElementById('searchButton').addEventListener('click', search);
                       searchResultsDiv.appendChild(recipeDiv);
                   });
               };
-
-
-document.getElementById('searchButton2').addEventListener('click', lookup);
-              
-              function lookup() {
-                  const searchTerm = encodeURIComponent(document.getElementById('searchInput').value);
-                  const ingredients = encodeURIComponent(document.getElementById('ingredientsInput').value);
-                  let baseURL = "https://api.api-ninjas.com/v1/cocktail";
-                  let myKey = "Fvgwx0GoSoc8O88Lryedl5EJyfpvRtdh9Zlbjr1k";
-                  let url = `${baseURL}?X-Api-Key=${myKey}&ingredients=${ingredients}`;
-
-                  fetch(url)
-                      .then(response => {
-                          if (!response.ok) {
-                              throw new Error('Network response was not ok');
-                          }
-                          return response.json();
-                      })
-                      .then(data => {
-                          displayIngredients(data); 
-                      })
-                      .catch(error => {
-                          console.error('There was a problem with the fetch operation:', error);
-                      });
-              }
-
-              function displayIngredients(data) {
-                  const ResultsDiv = document.getElementById('Results');
-                  ResultsDiv.innerHTML = ""; 
-                  data.forEach(recipe => {
-                      const ingDiv = document.createElement('div');
-                      ingDiv.textContent = `Name: ${recipe.name} Ingredients: ${recipe.ingredients}
-                      Instructions:${recipe.instructions}`; 
-                      ResultsDiv.appendChild(ingDiv);
-                  });
-              }
-      
-      
-
